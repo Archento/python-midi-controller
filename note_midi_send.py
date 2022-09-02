7cd883b9658db6005d399098857a42eea4c2affb
wildeflow <crossmail@gmx.de>
2022-09-02 14:41:58 +0200

    update midi out file

--- note_midi_send.py
+++ note_midi_send.py
@@ -1,31 +1,27 @@
 import time
 
-import rtmidi
+import rtmidi.midiutil as midi
 from rtmidi.midiconstants import NOTE_ON, NOTE_OFF
 
-midiout = rtmidi.MidiOut()
-available_ports = midiout.get_ports()
-# print(available_ports)
-
-
 def play_note(note, velocity=127, hold_time=1):
-    midiout.send_message([NOTE_ON, note, velocity]) # note ON
+    midi_out.send_message([NOTE_ON, note, velocity]) # note ON
     time.sleep(hold_time)
-    midiout.send_message([NOTE_OFF, note, 0]) # note OFF
+    midi_out.send_message([NOTE_OFF, note, 0]) # note OFF
 
 
 def play_chord(notes, velocity=127, hold_time=1):
     for note in notes:
-        midiout.send_message([NOTE_ON, note, velocity]) # note ON
+        midi_out.send_message([NOTE_ON, note, velocity]) # note ON
     time.sleep(hold_time)
     for note in notes:
-        midiout.send_message([NOTE_OFF, note, 0]) # note OFF
+        midi_out.send_message([NOTE_OFF, note, 0]) # note OFF
 
 
-midiout.open_port(2)
+midi_out, portname = midi.open_midioutput()
 
-with midiout:
+with midi_out:
     play_chord([60, 64, 67, 71], hold_time=3)
     time.sleep(0.1)
 
-del midiout
+midi_out.close_port()
+del midi_out
