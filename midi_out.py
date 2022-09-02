e6c4c6a8ab6947d59b297e4b4169918833261f56
claw-z <you@example.com>
2022-09-02 15:38:54 +0200

    rename file to midi_out, add comments

--- midi_out.py
+++ midi_out.py
@@ -1,14 +1,19 @@
+"""
+This module is an example for generating MIDI output.
+"""
 import time
-
+# Only modules 'midiutil' and 'midiconstants' are imported and assigned to var 'midi'
 import rtmidi.midiutil as midi
-from rtmidi.midiconstants import NOTE_ON, NOTE_OFF
+# variables imported from midiconstants for better readability of code
+from rtmidi.midiconstants import NOTE_OFF, NOTE_ON
 
+# function for one single note
 def play_note(note, velocity=127, hold_time=1):
     midi_out.send_message([NOTE_ON, note, velocity]) # note ON
     time.sleep(hold_time)
     midi_out.send_message([NOTE_OFF, note, 0]) # note OFF
 
-
+# function for multiple notes (notes = list)
 def play_chord(notes, velocity=127, hold_time=1):
     for note in notes:
         midi_out.send_message([NOTE_ON, note, velocity]) # note ON
@@ -16,7 +21,7 @@ def play_chord(notes, velocity=127, hold_time=1):
     for note in notes:
         midi_out.send_message([NOTE_OFF, note, 0]) # note OFF
 
-
+# destructured assignment of "open_midioutput"-tuple
 midi_out, portname = midi.open_midioutput()
 
 with midi_out:
