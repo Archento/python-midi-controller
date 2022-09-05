e11d1fa5eb61c0fdda5fced1b5a3f454bcfc4a75
claw-z <you@example.com>
2022-09-05 12:14:38 +0200

    change midi_in, change interface

--- interface.py
+++ interface.py
@@ -1,7 +1,8 @@
-import src.midi_out as m_out
-
+import time
 from tkinter import *
 
+import src.midi_in as m_in
+import src.midi_out as m_out
 
 app = Tk()
 app.title("Python MIDI Tool")
@@ -11,7 +12,8 @@ label = Label(text = "Test Label")
 label.pack()
 
 
-midi_output, portname = m_out.get_midi_out(2)
+midi_input, input_port_name = m_in.get_midi_in(0)
+midi_output, output_port_name = m_out.get_midi_out(2)
 
 
 def button_pressed():
@@ -19,8 +21,32 @@ def button_pressed():
     m_out.play_chord(midi_output, [60, 64, 67, 71], velocity=100, hold_time=3)
 
 
-button1 = Button(text = "Drück mir", command = button_pressed) 
+button1 = Button(text = "Test chord", command = button_pressed) 
 button1.pack()
+
+
+def press_listen():
+    print('Listen was pressed')
+    while True:
+        # Rückgabewert von get_message ist: (<Die Note>, <vergangene Zeit seit letztem Befehl>)
+        msg = midi_input.get_message()
+        if msg:
+            # Destrukturierung, weil wir nur einen Teil davon benutzen wollen
+            message, delta = msg
+            # Zugriff auf einzelne Parameter möglich (weitere Destrukturierung)
+            [event, note, velocity] = message
+            # keine Ausgabe, falls nicht im dict
+            if event not in m_in.channel_events:
+                continue
+            # 'channel_events' wird  benutzt, so dass statt Zahlen die jew. Bedeutungen angezeigt werden
+            print(f"channel: {m_in.channel_events[event]}, note: {note}, velocity: {velocity}")
+        # Polling-Intervall (Abfragen pro Sekunde werden begrenzt, um Ressourcen zu sparen) 
+        time.sleep(0.1)
+
+
+btn_listen = Button(text = "Listen", command = press_listen)
+btn_listen.pack()
+
 
 
 app.mainloop()
--- src/midi_in.py
+++ src/midi_in.py
@@ -7,16 +7,16 @@ import rtmidi.midiutil as midi
 
 
 # Methode open_midiinput wird ausgeführt und in zwei Variablen destrukturiert, die den jew. Werten des Tuples (= Rückgabewert von open_midiinput) zugewiesen werden
-def get_midi_in() -> tuple:
+def get_midi_in(port = None) -> tuple:
     """Generates MIDI input object using rtmidi-library
 
     Returns:
         tuple: rtmidi-object, port name
     """
-    return midi.open_midiinput()
+    return midi.open_midiinput(port)
 
 
-midi_in, port_name = get_midi_in()
+# midi_in, port_name = get_midi_in()
 
 
 # Schlüssel für Eventtypen
@@ -31,26 +31,26 @@ channel_events = {
 }
 
 # Einbettung des MIDI-Monitorings in try-except-Block
-try:
-    while True:
-        # Rückgabewert von get_message ist: (<Die Note>, <vergangene Zeit seit letztem Befehl>)
-        msg = midi_in.get_message()
-        if msg:
-            # Destrukturierung, weil wir nur einen Teil davon benutzen wollen
-            message, delta = msg
-            # Zugriff auf einzelne Parameter möglich (weitere Destrukturierung)
-            [event, note, velocity] = message
-            # keine Ausgabe, falls nicht im dict
-            if event not in channel_events:
-                continue
-            # 'channel_events' wird  benutzt, so dass statt Zahlen die jew. Bedeutungen angezeigt werden
-            print(f"channel: {channel_events[event]}, note: {note}, velocity: {velocity}")
-        # Polling-Intervall (Abfragen pro Sekunde werden begrenzt, um Ressourcen zu sparen) 
-        time.sleep(0.1)
-# Loop kann per ctrl+C gestoppt werden
-except KeyboardInterrupt: 
-    pass
-# MIDI-Port wieder schließen und Variable aus dem Speicher entfernen
-finally:    
-    midi_in.close_port()
-    del midi_in
+# try:
+#     while True:
+#         # Rückgabewert von get_message ist: (<Die Note>, <vergangene Zeit seit letztem Befehl>)
+#         msg = midi_in.get_message()
+#         if msg:
+#             # Destrukturierung, weil wir nur einen Teil davon benutzen wollen
+#             message, delta = msg
+#             # Zugriff auf einzelne Parameter möglich (weitere Destrukturierung)
+#             [event, note, velocity] = message
+#             # keine Ausgabe, falls nicht im dict
+#             if event not in channel_events:
+#                 continue
+#             # 'channel_events' wird  benutzt, so dass statt Zahlen die jew. Bedeutungen angezeigt werden
+#             print(f"channel: {channel_events[event]}, note: {note}, velocity: {velocity}")
+#         # Polling-Intervall (Abfragen pro Sekunde werden begrenzt, um Ressourcen zu sparen) 
+#         time.sleep(0.1)
+# # Loop kann per ctrl+C gestoppt werden
+# except KeyboardInterrupt: 
+#     pass
+# # MIDI-Port wieder schließen und Variable aus dem Speicher entfernen
+# finally:    
+#     midi_in.close_port()
+#     del midi_in
