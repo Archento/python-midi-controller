5e2d481b5b3badbc521424b5b269f8b7532ccb98
claw-z <you@example.com>
2022-09-06 09:20:45 +0200

    add set midi out port function; comments

--- interface.py
+++ interface.py
@@ -12,7 +12,7 @@ app.geometry("560x350")
 midi_input = None
 midi_output = None
 
-# INPUT #
+########## MIDI LISTEN ##########
 
 def press_listen():
     msg = midi_input.get_message()
@@ -31,7 +31,7 @@ btn_listen = ttk.Button(text = "Listen", command = press_listen)
 btn_listen.grid(row = 4, column = 0, padx = 20, sticky = "W")
 btn_listen.state(['disabled'])
 
-
+########## PLAY SOUND ########## 
 def play_sound():
     print('The button was pressed')
     # m_out.play_chord(midi_output, [60, 64, 67, 71], velocity=100, hold_time=3)
@@ -40,8 +40,7 @@ button1 = ttk.Button(text = "Test chord", command = play_sound)
 button1.grid(row = 5, column = 0, pady = 50)
 
 
-### LAYOUT ###
-# MIDI IN-PORT MENU #
+########## MIDI IN-PORT MENU ##########
 label_in_port = tk.Label(text = "MIDI In port:")
 label_in_port.grid(row = 0, column = 1, sticky = "E", pady = 20)
 
@@ -70,20 +69,46 @@ def set_midi_in_port():
 btn_set_midi_in = ttk.Button(text="Set", command=set_midi_in_port)
 btn_set_midi_in.grid(row = 0, column = 3)
 
-# MIDI OUT-PORT MENU #
 
+########## MIDI OUT-PORT MENU ##########
 label_out_port = tk.Label(text = "MIDI Out port:")
-label_out_port.grid(row = 1, column = 1)
+label_out_port.grid(row = 1, column = 1, sticky = "E")
+
+# Get dict of MIDI Out ports
+out_ports = {}
+for portno, name in enumerate(m_out.get_out_ports()):
+    out_ports[name] = portno
 
 midi_out_menu = ttk.Combobox(app)
+midi_out_menu['state'] = 'readonly'
+midi_out_menu["values"] = list(out_ports.keys())
+midi_out_menu.set(list(out_ports.keys())[0])
 midi_out_menu.grid(row = 1, column = 2)
 
-btn_set_midi_out = ttk.Button(text = "Set")
+
+def set_midi_out_port():
+    global midi_output
+    chosen_out_port = out_ports[midi_out_menu.get()]
+    midi_output, portname = m_out.get_midi_out(chosen_out_port)
+    showinfo(
+        title='Result',
+        message=f'You selected {portname} as your OUT port.'
+    )
+    btn_set_midi_out.state(['disabled'])
+    btn_listen.state(['!disabled'])
+    print(btn_listen.state)
+
+# Assign "set_midi_out_port" function to button
+btn_set_midi_out = ttk.Button(text = "Set", command = set_midi_out_port)
 btn_set_midi_out.grid(row = 1, column = 3)
 
+
+########## SEPARATOR ##########
 sep = ttk.Separator(app, orient=tk.HORIZONTAL)
 sep.grid(row = 2, column = 0, pady = 20, columnspan = 4, sticky=tk.EW)
 
+
+########## LABELS FOR MIDI IN MONITOR GRID ##########
 label_onoff = tk.Label(text = "Event")
 label_note = tk.Label(text = "Note")
 label_velocity = tk.Label(text = "Velocity")
@@ -99,5 +124,7 @@ field_velocity = tk.Label()
 field_onoff.grid(row = 4, column = 1, padx = 20, pady = 10)
 field_note.grid(row = 4, column = 2, padx = 20)
 field_velocity.grid(row = 4, column = 3, padx = 20)
+
+
 
 app.mainloop()
